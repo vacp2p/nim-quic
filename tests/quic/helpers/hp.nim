4128b33639e84cc20502016f2973c7866983d8c5
Mark Spanbroek <mark@spanbroek.net>
2020-09-15 13:16:33 +0200

    Use fake header protection mask

--- tests/quic/helpers/hp.nim
+++ tests/quic/helpers/hp.nim
@@ -1,6 +1,7 @@
 import ngtcp2
 
 proc dummyHpMask*(dest: ptr uint8, hp: ptr ngtcp2_crypto_cipher, hpContext: ptr ngtcp2_crypto_cipher_ctx, sample: ptr uint8): cint {.cdecl.} =
-  # memcpy(dest, NGTCP2_FAKE_HP_MASK, sizeof(NGTCP2_FAKE_HP_MASK) - 1);
   echo "HP MASK"
+  var NGTCP2_FAKE_HP_MASK = "\x00\x00\x00\x00\x00"
+  copyMem(dest, addr NGTCP2_FAKE_HP_MASK[0], NGTCP2_FAKE_HP_MASK.len)
 
