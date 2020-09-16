c1b45207762bd0df30be07df3aaa5b305b3f283c
Mark Spanbroek <mark@spanbroek.net>
2020-09-16 15:39:46 +0200

    Add PacketNumber

--- tests/quic/testPackets.nim
+++ tests/quic/testPackets.nim
@@ -1,6 +1,11 @@
 import unittest
 import quic
+import math
 
 test "there are two kinds of packet headers; short and long":
   discard PacketHeader(kind: headerShort)
   discard PacketHeader(kind: headerLong)
+
+test "packet numbers are in the range 0 to 2^62-1":
+  check PacketNumber.low == 0
+  check PacketNumber.high == 2'u64 ^ 62 - 1
