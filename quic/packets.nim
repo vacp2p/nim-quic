ce52086d4e5555de87942c2d17fd0290d0ed1bf9
Mark Spanbroek <mark@spanbroek.net>
2020-09-16 15:39:46 +0200

    Add PacketNumber

--- quic/packets.nim
+++ quic/packets.nim
@@ -1,6 +1,10 @@
+import math
+
 type
   PacketHeaderKind* = enum
     headerShort
     headerLong
   PacketHeader* = object
     kind*: PacketHeaderKind
+
+  PacketNumber* = range[0'u64..2'u64^62-1]
