db9febbb195e032ed17dddc7ca4f2bcce2385f0b
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 10:58:01 +0200

    Store its kind in the first bit of the header

--- tests/quic/testPackets.nim
+++ tests/quic/testPackets.nim
@@ -1,12 +1,18 @@
 import unittest
-import quic
 import math
+import quic
+import quic/bits
 
 suite "packets":
 
-  test "there are two kinds of packet headers; short and long":
-    discard PacketHeader(kind: headerShort)
-    discard PacketHeader(kind: headerLong)
+  test "first bit of the header indicates its kind":
+    check PacketHeader(bytes: @[0b00000000'u8]).kind == headerShort
+    check PacketHeader(bytes: @[0b10000000'u8]).kind == headerLong
+
+  test "header kind can be set":
+    var header = PacketHeader(bytes: @[0b00000000'u8])
+    header.kind = headerLong
+    check header.bytes[0].bits[0] == 1
 
   test "packet numbers are in the range 0 to 2^62-1":
     check PacketNumber.low == 0
