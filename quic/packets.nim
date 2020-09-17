2862e5b08c145c1f1c7758639e4899993c2c6f9c
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 10:58:01 +0200

    Store its kind in the first bit of the header

--- quic/packets.nim
+++ quic/packets.nim
@@ -1,10 +1,16 @@
 import math
+import bits
 
 type
   PacketHeaderKind* = enum
     headerShort
     headerLong
   PacketHeader* = object
-    kind*: PacketHeaderKind
-
+    bytes*: seq[byte]
   PacketNumber* = range[0'u64..2'u64^62-1]
+
+proc kind*(header: PacketHeader): PacketHeaderKind =
+  PacketHeaderKind(header.bytes[0].bits[0])
+
+proc `kind=`*(header: var PacketHeader, kind: PacketHeaderKind) =
+  header.bytes[0].bits[0] = Bit(kind)
