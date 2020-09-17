813b44f7120ceeefe3ee653560f236298b2dc165
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 14:22:25 +0200

    Slightly better ordering of procs

--- quic/packets.nim
+++ quic/packets.nim
@@ -13,11 +13,12 @@ proc newPacketHeader*(bytes: seq[byte]): PacketHeader =
   assert bytes[0].bits[1] == 1
   PacketHeader(bytes: bytes)
 
+proc bytes*(header: PacketHeader): seq[byte] =
+  header.bytes
+
 proc form*(header: PacketHeader): HeaderForm =
   HeaderForm(header.bytes[0].bits[0])
 
 proc `form=`*(header: var PacketHeader, form: HeaderForm) =
   header.bytes[0].bits[0] = Bit(form)
 
-proc bytes*(header: PacketHeader): seq[byte] =
-  header.bytes
