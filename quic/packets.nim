9ea715662be5d4a3f7023d17c832f729a5f22312
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 14:24:49 +0200

    Add QUIC version to packet header

--- quic/packets.nim
+++ quic/packets.nim
@@ -22,3 +22,14 @@ proc form*(header: PacketHeader): HeaderForm =
 proc `form=`*(header: var PacketHeader, form: HeaderForm) =
   header.bytes[0].bits[0] = Bit(form)
 
+proc version*(header: PacketHeader): uint32 =
+  result.bytes[0] = header.bytes[1]
+  result.bytes[1] = header.bytes[2]
+  result.bytes[2] = header.bytes[3]
+  result.bytes[3] = header.bytes[4]
+
+proc `version=`*(header: var PacketHeader, version: uint32) =
+  header.bytes[1] = version.bytes[0]
+  header.bytes[2] = version.bytes[1]
+  header.bytes[3] = version.bytes[2]
+  header.bytes[4] = version.bytes[3]
--- tests/quic/testPackets.nim
+++ tests/quic/testPackets.nim
@@ -18,6 +18,22 @@ suite "packets":
     expect Exception:
       discard newPacketHeader(@[0b00000000'u8])
 
+  test "QUIC version is stored in bytes 1..4":
+    var version = 0xAABBCCDD'u32
+    var header = newPacketHeader(@[
+      0b01000000'u8,
+      version.bytes[0],
+      version.bytes[1],
+      version.bytes[2],
+      version.bytes[3]
+    ])
+    check header.version == version
+
+  test "QUIC version can be set":
+    var header = newPacketHeader(@[0b01000000'u8, 0'u8, 0'u8, 0'u8, 0'u8])
+    header.version = 0xAABBCCDD'u32
+    check header.bytes[1..4] == @[0xAA'u8, 0xBB'u8, 0xCC'u8, 0xDD'u8]
+
   test "packet numbers are in the range 0 to 2^62-1":
     check PacketNumber.low == 0
     check PacketNumber.high == 2'u64 ^ 62 - 1
