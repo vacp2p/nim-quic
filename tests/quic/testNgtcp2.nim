196b0204f75b34a1ee930efc288e0b135da99aee
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 10:32:31 +0200

    Put all tests in suites

--- tests/quic/testNgtcp2.nim
+++ tests/quic/testNgtcp2.nim
@@ -1,8 +1,10 @@
 import unittest
 import ngtcp2
 
-test "default settings":
-  var settings: ngtcp2_settings
-  ngtcp2_settings_default(addr settings)
-  check settings.transport_params.max_udp_payload_size > 0
-  check settings.transport_params.active_connection_id_limit > 0
+suite "ngtcp2":
+
+  test "default settings":
+    var settings: ngtcp2_settings
+    ngtcp2_settings_default(addr settings)
+    check settings.transport_params.max_udp_payload_size > 0
+    check settings.transport_params.active_connection_id_limit > 0
