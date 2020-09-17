0ca8fd9f270cd969ab280de803b6986e23b7c11b
Mark Spanbroek <mark@spanbroek.net>
2020-09-17 10:32:31 +0200

    Put all tests in suites

--- tests/quic/testPackets.nim
+++ tests/quic/testPackets.nim
@@ -2,10 +2,12 @@ import unittest
 import quic
 import math
 
-test "there are two kinds of packet headers; short and long":
-  discard PacketHeader(kind: headerShort)
-  discard PacketHeader(kind: headerLong)
+suite "packets":
 
-test "packet numbers are in the range 0 to 2^62-1":
-  check PacketNumber.low == 0
-  check PacketNumber.high == 2'u64 ^ 62 - 1
+  test "there are two kinds of packet headers; short and long":
+    discard PacketHeader(kind: headerShort)
+    discard PacketHeader(kind: headerLong)
+
+  test "packet numbers are in the range 0 to 2^62-1":
+    check PacketNumber.low == 0
+    check PacketNumber.high == 2'u64 ^ 62 - 1
