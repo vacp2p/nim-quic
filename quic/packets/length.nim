60af5afb2e069354591ac919457c20068d7048a6
Mark Spanbroek <mark@spanbroek.net>
2020-09-23 12:17:40 +0200

    Rename: packetLength -> len

--- quic/packets/length.nim
+++ quic/packets/length.nim
@@ -2,7 +2,7 @@ import packet
 
 {.push raises:[].} # avoid exceptions in this module
 
-proc packetLength*(packet: Packet): int =
+proc len*(packet: Packet): int =
   case packet.kind:
   of packetVersionNegotiation:
     return 11 + packet.destination.len + packet.source.len
