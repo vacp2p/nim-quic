f7db532ed5091f4d8963268d30a2dfbc5a4afbc7
Mark Spanbroek <mark@spanbroek.net>
2020-09-28 09:38:27 +0200

    Introduce reader state

--- quic/packets/read.nim
+++ quic/packets/read.nim
@@ -6,83 +6,70 @@ import packet
 
 {.push raises:[].} # avoid exceptions in this module
 
-proc readForm*(datagram: Datagram): PacketForm =
-  PacketForm(datagram[0].bits[0])
+type
+  PacketReader* = object
+    first, next: int
 
-proc readFixedBit*(datagram: Datagram) =
-  doAssert datagram[0].bits[1] == 1
+proc peek(reader: var PacketReader, datagram: Datagram, amount: int): seq[byte] =
+  datagram[reader.next..<reader.next+amount]
 
-proc readVersion*(datagram: Datagram): uint32 =
-  fromBytesBE(uint32, datagram[1..4])
+proc move(reader: var PacketReader, amount: int) =
+  reader.next = reader.next + amount
 
-proc readKind*(datagram: Datagram): PacketKind =
-  if datagram.readVersion() == 0:
+proc read*(reader: var PacketReader, datagram: Datagram, amount: int): seq[byte] =
+  result = reader.peek(datagram, amount)
+  reader.move(amount)
+
+proc read(reader: var PacketReader, datagram: Datagram): byte =
+  result = datagram[reader.next]
+  reader.move(1)
+
+proc readVarInt*(reader: var PacketReader, datagram: Datagram): VarIntCompatible =
+  result = fromVarInt(datagram.toOpenArray(reader.next, datagram.len-1))
+  reader.move(varintlen(datagram.toOpenArray(reader.next, datagram.len-1)))
+
+proc readForm*(reader: var PacketReader, datagram: Datagram): PacketForm =
+  PacketForm(datagram[reader.next].bits[0])
+
+proc readFixedBit*(reader: var PacketReader, datagram: Datagram) =
+  doAssert datagram[reader.next].bits[1] == 1
+
+proc peekVersion*(reader: var PacketReader, datagram: Datagram): uint32 =
+  fromBytesBE(uint32, reader.peek(datagram, 4))
+
+proc readVersion*(reader: var PacketReader, datagram: Datagram): uint32 =
+  fromBytesBE(uint32, reader.read(datagram, 4))
+
+proc readKind*(reader: var PacketReader, datagram: Datagram): PacketKind =
+  var kind: uint8
+  kind.bits[6] = datagram[reader.next].bits[2]
+  kind.bits[7] = datagram[reader.next].bits[3]
+  reader.move(1)
+  if reader.peekVersion(datagram) == 0:
     packetVersionNegotiation
   else:
-    var kind: uint8
-    kind.bits[6] = datagram[0].bits[2]
-    kind.bits[7] = datagram[0].bits[3]
     PacketKind(kind)
 
-proc findDestination(datagram: Datagram): Slice[int] =
-  let start = 6
-  let length = datagram[5].int
-  start..<start+length
+proc readConnectionId*(reader: var PacketReader, datagram: Datagram): ConnectionId =
+  let length = reader.read(datagram).int
+  ConnectionId(reader.read(datagram, length))
 
-proc findSource(datagram: Datagram): Slice[int] =
-  let destinationEnd = datagram.findDestination().b + 1
-  let start = destinationEnd + 1
-  let length = datagram[destinationEnd].int
-  start..<start+length
+proc readToken*(reader: var PacketReader, datagram: Datagram): seq[byte] =
+  let length = datagram.len - 16 - reader.next
+  reader.read(datagram, length)
 
-proc findSupportedVersion(datagram: Datagram): Slice[int] =
-  let start = datagram.findSource().b + 1
-  start..<start+4
-
-proc findPacketLength(datagram: Datagram): Slice[int] =
-  let start = datagram.findSource().b + 1
-  let length = varintlen(datagram.toOpenArray(start, datagram.len-1))
-  start..<start+length
-
-proc findPacketNumber(datagram: Datagram): Slice[int] =
-  let start = datagram.findPacketLength().b + 1
-  let length = 1 + int(datagram[0] and 0b11)
-  start..<start+length
-
-proc findPayload(datagram: Datagram): Slice[int] =
-  let start = datagram.findPacketNumber().b + 1
-  let length = datagram[datagram.findPacketLength()].fromVarInt
-  start..<start+length.int
-
-proc readDestination*(datagram: Datagram): ConnectionId =
-  ConnectionId(datagram[datagram.findDestination()])
-
-proc readSource*(datagram: Datagram): ConnectionId =
-  ConnectionId(datagram[datagram.findSource()])
-
-proc readSupportedVersion*(datagram: Datagram): uint32 =
-  let versionBytes = datagram[datagram.findSupportedVersion()]
-  fromBytesBE(uint32, versionBytes)
-
-proc readToken*(datagram: Datagram): seq[byte] =
-  let start = datagram.findSource().b + 1
-  let stop = datagram.len-16
-  datagram[start..<stop]
-
-proc readIntegrity*(datagram: Datagram): array[16, byte] =
+proc readIntegrity*(reader: var PacketReader, datagram: Datagram): array[16, byte] =
   try:
-    result[0..<16] = datagram[datagram.len-16..<datagram.len]
+    result[0..<16] = reader.read(datagram, 16)
   except RangeError:
     doAssert false, "programmer error: assignment ranges do not match"
 
-proc readPacketNumber*(datagram: Datagram): PacketNumber =
-  let bytes = datagram[datagram.findPacketNumber()]
+proc readPacketNumber*(reader: var PacketReader, datagram: Datagram): PacketNumber =
+  let length = 1 + int(datagram[reader.first] and 0b11)
+  let bytes = reader.read(datagram, length)
   var padded: array[8, byte]
   try:
     padded[padded.len-bytes.len..<padded.len] = bytes
   except RangeError:
     doAssert false, "programmer error: assignment ranges do not match"
   fromBytesBE(uint64, padded)
-
-proc readPayload*(datagram: Datagram): seq[byte] =
-  datagram[datagram.findPayload()]
