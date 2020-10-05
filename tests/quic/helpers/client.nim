5728c3d5a009031404977bc3911aecd1aed94f65
Mark Spanbroek <mark@spanbroek.net>
2020-10-05 16:59:52 +0200

    Cleanup: extract procs to install keys for 0-RTT, handshake and 1-RTT

--- tests/quic/helpers/client.nim
+++ tests/quic/helpers/client.nim
@@ -7,22 +7,12 @@ import hp
 import keys
 import settings
 
+let zeroKey = Key()
 var cryptoData: array[4096, uint8]
-
 var randomId: ngtcp2_cid
 
 proc clientInitial(connection: ptr ngtcp2_conn, user_data: pointer): cint {.cdecl.} =
-  var initialKey: Key
-  assert 0 == ngtcp2_conn_install_initial_key(
-    connection,
-    addr initialKey.aeadContext,
-    addr initialKey.iv[0],
-    addr initialKey.hpContext,
-    addr initialKey.aeadContext,
-    addr initialKey.iv[0],
-    addr initialKey.hpContext,
-    sizeof(initialKey.iv).uint
-  )
+  connection.install0RttKey(zeroKey)
 
   assert 0 == ngtcp2_conn_submit_crypto_data(
     connection, NGTCP2_CRYPTO_LEVEL_INITIAL, addr cryptoData[0], sizeof(cryptoData).uint
@@ -34,23 +24,7 @@ proc receiveCryptoData(connection: ptr ngtcp2_conn, level: ngtcp2_crypto_level, 
   params.original_dcid = randomId
   assert 0 == ngtcp2_conn_set_remote_transport_params(connection, addr params)
 
-  var rxHandshakeKey: Key
-  assert 0 == ngtcp2_conn_install_rx_handshake_key(
-    connection,
-    addr rxHandshakeKey.aeadContext,
-    addr rxHandshakeKey.iv[0],
-    sizeof(rxHandshakeKey.iv).uint,
-    addr rxHandshakeKey.hpContext
-  )
-
-  var txHandshakeKey: Key
-  assert 0 == ngtcp2_conn_install_tx_handshake_key(
-    connection,
-    addr txHandshakeKey.aeadContext,
-    addr txHandshakeKey.iv[0],
-    sizeof(txHandshakeKey.iv).uint,
-    addr txHandshakeKey.hpContext
-  )
+  connection.installHandshakeKeys(zeroKey, zeroKey)
 
   ngtcp2_conn_handshake_completed(connection)
 
@@ -67,27 +41,7 @@ proc handshakeConfirmed(conn: ptr ngtcp2_conn, userData: pointer): cint {.cdecl.
   discard
 
 proc handshakeCompleted(connection: ptr ngtcp2_conn, userData: pointer): cint {.cdecl.} =
-  var rxKey: Key
-  assert 0 == ngtcp2_conn_install_rx_key(
-    connection,
-    addr rxKey.secret[0],
-    rxKey.secret.len.uint,
-    addr rxKey.aeadContext,
-    addr rxKey.iv[0],
-    rxKey.iv.len.uint,
-    addr rxKey.hpContext
-  )
-
-  var txKey: Key
-  assert 0 == ngtcp2_conn_install_tx_key(
-    connection,
-    addr txKey.secret[0],
-    txKey.secret.len.uint,
-    addr txKey.aeadContext,
-    addr txKey.iv[0],
-    txKey.iv.len.uint,
-    addr txKey.hpContext
-  )
+  connection.install1RttKeys(zeroKey, zeroKey)
 
 proc setupClient*(path: ptr ngtcp2_path, sourceId: ptr ngtcp2_cid, destinationId: ptr ngtcp2_cid): ptr ngtcp2_conn =
   randomId = destinationId[]
