81cfe6413787ba3204f06ae2af25d8ba61ed3a99
Mark Spanbroek <mark@spanbroek.net>
2020-10-06 12:00:59 +0200

    Communicate transport parameters via encrypted payload

--- quic/openarray.nim
+++ quic/openarray.nim
@@ -6,3 +6,6 @@ proc `[]=`*[T,U,V](
   doAssert replacement.len == slice.len
   for i in 0..<replacement.len:
     target[slice.a + i] = replacement[i]
+
+template toOpenArray*[T](a: ptr T, length: uint): openArray[T] =
+  toOpenArray(cast[ptr UncheckedArray[T]](a), 0, length.int-1)
