a69e5a16a8bee019fda5d6b24056dd46b82e887a
Mark Spanbroek <mark@spanbroek.net>
2020-10-07 16:41:45 +0200

    Update to newer version of nim-ngtcp2
    It no longer has the SocketAddress object, but uses the objects
    from nativesockets instead.

--- tests/quic/helpers/path.nim
+++ tests/quic/helpers/path.nim
@@ -1,9 +1,10 @@
 import ngtcp2
+import nativesockets
 
-var zeroAddress: SocketAddress
-zeroAddress.ipv4.sin_family = AF_INET
+var zeroAddress: Sockaddr
+zeroAddress.sa_family = AF_INET.uint16
 
 let zeroPath* = ngtcp2_path(
-  local: ngtcp2_addr(`addr`: addr zeroAddress.address, addrlen: sizeof(SocketAddress).uint),
-  remote: ngtcp2_addr(`addr`: addr zeroAddress.address, addrlen: sizeof(SocketAddress).uint)
+  local: ngtcp2_addr(`addr`: addr zeroAddress, addrlen: sizeof(SockAddr).uint),
+  remote: ngtcp2_addr(`addr`: addr zeroAddress, addrlen: sizeof(SockAddr).uint)
 )
