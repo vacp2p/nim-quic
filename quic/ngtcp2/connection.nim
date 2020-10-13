217c5e6701308fd11831a9ac152240823e5a8eb0
Mark Spanbroek <mark@spanbroek.net>
2020-10-13 15:36:00 +0200

    Fix: Connection should be passed by reference to avoid double free

--- quic/ngtcp2/connection.nim
+++ quic/ngtcp2/connection.nim
@@ -1,11 +1,13 @@
 import ngtcp2
 import path
 
-type Connection* = object
-  conn*: ptr ngtcp2_conn
-  path*: Path
+type
+  Connection* = ref ConnectionObj
+  ConnectionObj = object
+    conn*: ptr ngtcp2_conn
+    path*: Path
 
-proc `=destroy`*(connection: var Connection) =
+proc `=destroy`*(connection: var ConnectionObj) =
   if connection.conn != nil:
     ngtcp2_conn_del(connection.conn)
     connection.conn = nil
