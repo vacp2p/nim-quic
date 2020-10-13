a3d11ad2bacf7deb7c82e4b81e79c38043d8b5d5
Mark Spanbroek <mark@spanbroek.net>
2020-10-13 15:35:00 +0200

    Make congestion notification optional in the API

--- quic/congestion.nim
+++ quic/congestion.nim
@@ -1,5 +1,5 @@
 type
-  ExplicitCongestionNotification {.size: 1.} = enum
+  ExplicitCongestionNotification* {.size: 1.} = enum
     ecnNonCapable = 0b00
     ecnCapable1 = 0b01
     ecnCapable0 = 0b10
