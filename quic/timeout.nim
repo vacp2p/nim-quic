15973992cc0e3ff540b5d34b224d3e4edece39b7
Mark Spanbroek <mark@spanbroek.net>
2020-11-12 10:28:06 +0100

    Timeout can be set to a moment in time

--- quic/timeout.nim
+++ quic/timeout.nim
@@ -5,11 +5,11 @@ type Timeout* = ref object
   onExpiry: proc () {.gcsafe.}
   expired: AsyncEvent
 
-proc setTimer(timeout: Timeout, duration: Duration) =
+proc setTimer(timeout: Timeout, moment: Moment) =
   proc onTimeout(_: pointer) =
     timeout.expired.fire()
     timeout.onExpiry()
-  timeout.timer = setTimer(Moment.fromNow(duration), onTimeout)
+  timeout.timer = setTimer(moment, onTimeout)
 
 const skip = proc () = discard
 
@@ -20,10 +20,13 @@ proc stop*(timeout: Timeout) =
   if not timeout.timer.isNil:
     timeout.timer.clearTimer()
 
-proc set*(timeout: Timeout, duration: Duration) =
+proc set*(timeout: Timeout, moment: Moment) =
   timeout.stop()
   timeout.expired.clear()
-  timeout.setTimer(duration)
+  timeout.setTimer(moment)
+
+proc set*(timeout: Timeout, duration: Duration) =
+  timeout.set(Moment.fromNow(duration))
 
 proc expired*(timeout: Timeout) {.async.} =
   await timeout.expired.wait()
