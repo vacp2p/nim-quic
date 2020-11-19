861136c5718cdb484bee412802f978aa9825cfd5
Mark Spanbroek <mark@spanbroek.net>
2020-11-19 10:20:00 +0100

    Introduce proc that installs handshake callback

--- quic/ngtcp2/handshake.nim
+++ quic/ngtcp2/handshake.nim
@@ -1,5 +1,13 @@
+import chronos
 import ngtcp2
 import connection
 
 proc isHandshakeCompleted*(connection: Connection): bool =
   ngtcp2_conn_get_handshake_completed(connection.conn).bool
+
+proc onHandshakeCompleted(connection: ptr ngtcp2_conn,
+                          userData: pointer): cint {.cdecl.} =
+  cast[Connection](userData).handshake.fire()
+
+proc installHandshakeCallback*(callbacks: var ngtcp2_conn_callbacks) =
+  callbacks.handshake_completed = onHandshakeCompleted
