2154437c9325955ebe7135ed09072109c6e9736c
Mark Spanbroek <mark@spanbroek.net>
2020-12-02 10:26:29 +0100

    Prepare listener for upcoming addition of a connection table

--- quic/listener.nim
+++ quic/listener.nim
@@ -1,20 +1,34 @@
 import pkg/chronos
 import ./connection
+import ./connectionid
 
 type
   Listener* = ref object
     udp*: DatagramTransport
     incoming*: AsyncQueue[Connection]
-    connection*: Connection
+    connection: Connection
 
 proc newListener*: Listener =
   Listener(incoming: newAsyncQueue[Connection]())
+
+proc hasConnection(listener: Listener): bool =
+  not listener.connection.isNil
+
+proc getConnection(listener: Listener): Connection =
+  listener.connection
+
+proc addConnection(listener: Listener, connection: Connection) {.async.} =
+  listener.connection = connection
+  await listener.incoming.put(connection)
 
 proc getOrCreateConnection*(listener: Listener,
                             udp: DatagramTransport,
                             remote: TransportAddress):
                             Future[Connection] {.async.} =
-  if listener.connection.isNil:
-    listener.connection = newIncomingConnection(udp, remote)
-    await listener.incoming.put(listener.connection)
-  result = listener.connection
+  var connection: Connection
+  if not listener.hasConnection():
+    connection = newIncomingConnection(udp, remote)
+    await listener.addConnection(connection)
+  else:
+    connection = listener.getConnection()
+  result = connection
