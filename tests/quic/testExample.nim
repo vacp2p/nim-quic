4d226f7906392266d93f9c1e039288c9bf425d05
Mark Spanbroek <mark@spanbroek.net>
2020-12-16 10:47:50 +0100

    Add unit test for example in Readme

---
+++ tests/quic/testExample.nim
@@ -0,0 +1,29 @@
+import std/unittest
+import ../helpers/asynctest
+import pkg/quic
+import pkg/chronos
+
+suite "examples from Readme":
+
+  test "outgoing and incoming connections":
+
+    proc outgoing {.async.} =
+      let connection = await dial(initTAddress("127.0.0.1:12345"))
+      let stream = await connection.openStream()
+      let message = cast[seq[byte]]("some message")
+      await stream.write(message)
+      await stream.close()
+      await connection.close()
+
+    proc incoming {.async.} =
+      let listener = listen(initTAddress("127.0.0.1:12345"))
+      let connection = await listener.accept()
+      let stream = await connection.incomingStream()
+      let message = await stream.read()
+      await stream.close()
+      await connection.close()
+      await listener.stop()
+
+      check message == cast[seq[byte]]("some message")
+
+    waitFor allSucceeded(incoming(), outgoing())
