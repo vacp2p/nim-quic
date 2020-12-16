400f1c3ef546e0b290101d409032a253b624d876
Mark Spanbroek <mark@spanbroek.net>
2020-12-16 10:47:38 +0100

    Add allSucceeded() to async test helper.

--- tests/helpers/asynctest.nim
+++ tests/helpers/asynctest.nim
@@ -1,3 +1,5 @@
+import pkg/chronos
+
 template asynctest*(name, body) =
   test name:
     let asyncproc = proc {.async.} = body
@@ -19,3 +21,11 @@ template asyncteardown*(body) =
   teardown:
     let asyncproc = proc {.async.} = body
     waitFor asyncproc()
+
+proc allSucceeded*[T](futures: varargs[Future[T]]): Future[void] =
+  ## Returns a future which will complete only when all futures in are
+  ## completed.  When a future raises an exception or is cancelled, a
+  ## ``Defect`` will be raised.
+  for future in futures:
+    asyncSpawn(future)
+  allFutures(futures)
