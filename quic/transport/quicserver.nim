d97ceef32615bcf6510a26a18846fb33f8603617
Mark Spanbroek <mark@spanbroek.net>
2020-12-16 16:58:49 +0100

    Use Datagram parameter instead of openArray

--- quic/transport/quicserver.nim
+++ quic/transport/quicserver.nim
@@ -1,11 +1,12 @@
 import pkg/chronos
+import ../udp/datagram
 import ./quicconnection
 import ./ngtcp2/connection/openstate
 import ./ngtcp2/server
 
 proc newQuicServerConnection*(local, remote: TransportAddress,
-                              datagram: openArray[byte]): QuicConnection =
-  let server = newNgtcp2Server(local, remote, datagram)
+                              datagram: Datagram): QuicConnection =
+  let server = newNgtcp2Server(local, remote, datagram.data)
   let state = newOpenConnection(server)
   let connection = newQuicConnection(state)
   server.outgoing = connection.outgoing
