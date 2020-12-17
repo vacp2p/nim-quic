2e38637f716893d4964b3e624e3741fb38bdaa08
Mark Spanbroek <mark@spanbroek.net>
2020-12-17 12:17:25 +0100

    Replace outgoing queue by callback in Ngtcp2Connection

--- quic/transport/quicserver.nim
+++ quic/transport/quicserver.nim
@@ -4,12 +4,15 @@ import ./quicconnection
 import ./ngtcp2/connection/openstate
 import ./ngtcp2/server
 
+proc onSend(connection: QuicConnection, datagram: Datagram) =
+  connection.outgoing.putNoWait(datagram)
+
 proc newQuicServerConnection*(local, remote: TransportAddress,
                               datagram: Datagram): QuicConnection =
   let server = newNgtcp2Server(local, remote, datagram.data)
   let state = newOpenConnection(server)
   let connection = newQuicConnection(state)
-  server.outgoing = connection.outgoing
+  server.onSend = proc (datagram: Datagram) = connection.onSend(datagram)
   server.incoming = connection.incoming
   server.handshake = connection.handshake
   connection
