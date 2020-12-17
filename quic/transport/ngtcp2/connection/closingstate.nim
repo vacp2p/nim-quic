e9d44eadb68bd510794b3c678622ac9898a7d37a
Mark Spanbroek <mark@spanbroek.net>
2020-12-17 14:14:56 +0100

    Split DrainingConnection and ClosingConnection

--- quic/transport/ngtcp2/connection/closingstate.nim
+++ quic/transport/ngtcp2/connection/closingstate.nim
@@ -2,58 +2,21 @@ import pkg/chronos
 import ../../../udp/datagram
 import ../../quicconnection
 import ../../connectionid
-import ../../stream
-import ../../timeout
-import ./closedstate
+import ./drainingstate
 
 type
-  ClosingConnection* = ref object of ConnectionState
-    connection: QuicConnection
+  ClosingConnection* = ref object of DrainingConnection
     finalDatagram: Datagram
-    ids: seq[ConnectionId]
-    timeout: Timeout
-    duration: Duration
-    done: AsyncEvent
 
 proc newClosingConnection*(finalDatagram: Datagram, ids: seq[ConnectionId],
                            duration: Duration): ClosingConnection =
-  ClosingConnection(
-    finalDatagram: finalDatagram,
-    ids: ids,
-    duration: duration,
-    done: newAsyncEvent()
-  )
-
-proc onTimeout(state: ClosingConnection) =
-  state.done.fire()
+  let state = ClosingConnection(finalDatagram: finalDatagram)
+  state.init(ids, duration)
+  state
 
 {.push locks: "unknown".}
 
-method enter(state: ClosingConnection, connection: QuicConnection) =
-  state.connection = connection
-  state.timeout = newTimeout(proc = state.onTimeout())
-  state.timeout.set(state.duration)
-
-method leave(state: ClosingConnection) =
-  state.timeout.stop()
-  state.connection = nil
-
-method ids(state: ClosingConnection): seq[ConnectionId] =
-  state.ids
-
-method send(state: ClosingConnection) =
-  raise newException(ClosedConnectionError, "connection is closing")
-
 method receive(state: ClosingConnection, datagram: Datagram) =
   state.connection.outgoing.putNoWait(state.finalDatagram)
-
-method openStream(state: ClosingConnection): Future[Stream] {.async.} =
-  raise newException(ClosedConnectionError, "connection is closing")
-
-method close(state: ClosingConnection) {.async.} =
-  await state.done.wait()
-
-method drop(state: ClosingConnection) =
-  state.connection.switch(newClosedConnection())
 
 {.pop.}
