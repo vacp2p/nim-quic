29c07cb6d72bb5ffd2a24bff793f38d6072e57d9
Mark Spanbroek <mark@spanbroek.net>
2021-06-09 17:01:13 +0200

    Set ngtcp2 callbacks when entering open connection state

--- quic/transport/quicclientserver.nim
+++ quic/transport/quicclientserver.nim
@@ -1,22 +1,12 @@
 import pkg/chronos
-import ../helpers/errorasdefect
 import ../udp/datagram
 import ./quicconnection
-import ./stream
 import ./ngtcp2/native
 import ./ngtcp2/connection/openstate
 
 proc newConnection(ngtcp2Connection: Ngtcp2Connection): QuicConnection =
   let state = newOpenConnection(ngtcp2Connection)
   let connection = newQuicConnection(state)
-  ngtcp2Connection.onSend = proc(datagram: Datagram) =
-    errorAsDefect:
-      connection.outgoing.putNoWait(datagram)
-
-  ngtcp2Connection.onIncomingStream = proc(stream: Stream) =
-    connection.incoming.putNoWait(stream)
-  ngtcp2Connection.onHandshakeDone = proc =
-    connection.handshake.fire()
   connection
 
 proc newQuicClientConnection*(local, remote: TransportAddress): QuicConnection =
