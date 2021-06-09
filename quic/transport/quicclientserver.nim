3f3d8fdba82865ad0d153f6c9832cad7b1ff9533
Mark Spanbroek <mark@spanbroek.net>
2021-06-09 17:30:26 +0200

    Knowledge about ngtcp2 is moved down into the state machine

--- quic/transport/quicclientserver.nim
+++ quic/transport/quicclientserver.nim
@@ -1,17 +1,11 @@
 import pkg/chronos
 import ../udp/datagram
 import ./quicconnection
-import ./ngtcp2/native
 import ./ngtcp2/connection/openstate
 
-proc newConnection(ngtcp2Connection: Ngtcp2Connection): QuicConnection =
-  let state = newOpenConnection(ngtcp2Connection)
-  let connection = newQuicConnection(state)
-  connection
-
 proc newQuicClientConnection*(local, remote: TransportAddress): QuicConnection =
-  newConnection(newNgtcp2Client(local, remote))
+  newQuicConnection(openClientConnection(local, remote))
 
 proc newQuicServerConnection*(local, remote: TransportAddress,
                               datagram: Datagram): QuicConnection =
-  newConnection(newNgtcp2Server(local, remote, datagram.data))
+  newQuicConnection(openServerConnection(local, remote, datagram))
