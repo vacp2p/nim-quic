8801a6f31d7ae20a21a6d392e2c82d28d06f55e7
Richard Ramos <info@richardramos.me>
2025-03-10 14:12:47 -0400

    fix: use correct quic version

--- quic/transport/version.nim
+++ quic/transport/version.nim
@@ -1,3 +1,4 @@
 #from pkg/ngtcp2 import NGTCP2_PROTO_VER_V2
 
-const CurrentQuicVersion* = cast[uint32](0x6b3343cfu) # TODO: figure out how to export NGTCP2_PROTO_VER_V2 with futhark
+# V1
+const CurrentQuicVersion* =  cast[uint32](0x00000001u) # TODO: figure out how to export NGTCP2_PROTO_VER_V1 with futhark
