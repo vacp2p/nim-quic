beea14a46152415231911ed576eef0b718b886e5
Richard Ramos <info@richardramos.me>
2025-04-10 11:33:08 -0400

    fix: code review

--- quic/transport/framesorter.nim
+++ quic/transport/framesorter.nim
@@ -1,54 +1,19 @@
 import ../errors
-import std/[algorithm, strformat, tables]
+import std/[strformat, tables]
 import chronos
 
-const streamEnd = high(uint64)
-
 type
-  Gap = object
-    startPos: uint64
-    endPos: uint64 # endPos is exclusive
-
   FrameSorter* = object
     buffer: Table[uint64, byte] # sparse byte storage
-    gaps: seq[Gap] # list of missing ranges
     readPos: uint64 # where to emit data from
     incoming: AsyncQueue[seq[byte]]
-    finReceived: bool # true if stream sent FIN
+    lastPos: Opt[uint64] # contains the index for the last position for a stream once a FIN is received
 
 proc initFrameSorter*(incoming: AsyncQueue[seq[byte]]): FrameSorter =
   result.incoming = incoming
-  result.gaps = @[Gap(startPos: 0, endPos: streamEnd)] # start with [0..∞)
   result.buffer = initTable[uint64, byte]()
   result.readPos = 0
-
-proc normalizeGaps(gaps: var seq[Gap]) =
-  gaps.sort(
-    proc(a, b: Gap): int =
-      return cmp(a.startPos, b.startPos)
-  )
-  var merged: seq[Gap]
-  for gap in gaps:
-    if merged.len == 0 or merged[^1].endPos < gap.startPos:
-      merged.add gap
-    else:
-      merged[^1].endPos = max(merged[^1].endPos, gap.endPos)
-  gaps = merged
-
-proc removeGap(gaps: var seq[Gap], startPos, endPos: uint64) =
-  ## Remove [startPos, endPos) from gaps
-  var newGaps: seq[Gap]
-  for gap in gaps:
-    if endPos <= gap.startPos or startPos >= gap.endPos:
-      # No overlap
-      newGaps.add gap
-    else:
-      # Overlap exists
-      if startPos > gap.startPos:
-        newGaps.add Gap(startPos: gap.startPos, endPos: startPos)
-      if endPos < gap.endPos:
-        newGaps.add Gap(startPos: endPos, endPos: gap.endPos)
-  gaps = newGaps
+  result.lastPos = Opt.none(uint64)
 
 proc insert*(fs: var FrameSorter, offset: uint64, data: openArray[byte], isFin: bool) =
   ## Insert bytes into sparse buffer
@@ -65,13 +30,7 @@ proc insert*(fs: var FrameSorter, offset: uint64, data: openArray[byte], isFin: 
 
   if isFin:
     # Remove any data received after the end
-    fs.finReceived = true
-    removeGap(fs.gaps, offset + uint64(data.len), streamEnd)
-  else:
-    # Remove filled range from gaps
-    removeGap(fs.gaps, offset, offset + uint64(data.len))
-
-  normalizeGaps(fs.gaps)
+    fs.lastPos = Opt.some(offset + uint64(data.len) - 1)
 
   # Try to emit contiguous data
   var emitData: seq[byte]
@@ -90,10 +49,7 @@ proc insert*(fs: var FrameSorter, offset: uint64, data: openArray[byte], isFin: 
 #       ngtcp2_conn_shutdown_stream will also need to be replaced
 #       by a normal write with FIN flag
 proc isComplete*(fs: FrameSorter): bool =
-  ## True if all data is received and fin flag is seen
-  if not fs.finReceived:
+  if fs.lastPos.isNone:
     return false
-  for gap in fs.gaps:
-    if gap.startPos < streamEnd:
-      return false
-  return true
+
+  return  fs.readPos > fs.lastPos.get()
