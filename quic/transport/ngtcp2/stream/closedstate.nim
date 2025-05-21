ba94933d1ff5b20cc082b6acb43dc26aed4e0871
Richard Ramos <info@richardramos.me>
2025-05-21 15:57:37 -0400

    fix: code review

--- quic/transport/ngtcp2/stream/closedstate.nim
+++ quic/transport/ngtcp2/stream/closedstate.nim
@@ -30,7 +30,7 @@ method read*(state: ClosedStream): Future[seq[byte]] {.async.} =
   except AsyncQueueEmptyError:
     discard
   trace "cant read, stream is closed"
-  raise newException(StreamError, "stream is closed")
+  raise newException(ClosedStreamError, "stream is closed")
 
 method write*(state: ClosedStream, bytes: seq[byte]) {.async.} =
   trace "cant write, stream is closed"
