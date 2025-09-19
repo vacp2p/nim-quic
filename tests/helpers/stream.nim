c254f67a53e6c9ada01f763bd5e0ef630907db01
Vlado Pajić <vlado@brrrrr.at>
2025-09-19 13:00:59 +0200

    fix

--- tests/helpers/stream.nim
+++ tests/helpers/stream.nim
@@ -1,4 +1,5 @@
 import pkg/chronos
+import pkg/unittest2
 import pkg/quic/transport/stream
 
 proc newData*(size: int, val: uint8 = uint8(0xEE)): seq[uint8] =
@@ -22,7 +23,7 @@ proc readStreamTillEOF*(
       break
   return receivedData
 
-template checkEqual*(a: seq[byte], b: seq[byte]) =
+proc checkEqual*(a: seq[byte], b: seq[byte]) =
   if a.len != b.len:
     checkpoint("sequences do not have the same length: " & $a.len & " != " & $b.len)
     fail()
