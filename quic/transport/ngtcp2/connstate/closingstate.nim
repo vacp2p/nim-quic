7b67ad412c59746c4f9b6648992a011ed5ece481
Vlado Pajić <vlado@brrrrr.at>
2025-10-07 12:48:51 +0200

    style fix

--- quic/transport/ngtcp2/connstate/closingstate.nim
+++ quic/transport/ngtcp2/connstate/closingstate.nim
@@ -30,5 +30,7 @@ method enter(
   procCall enter(DrainingConnection(state), connection)
   state.sendFinalDatagram()
 
-method receive(state: ClosingConnection, datagram: sink Datagram) {.raises: [QuicError].} =
+method receive(
+    state: ClosingConnection, datagram: sink Datagram
+) {.raises: [QuicError].} =
   state.sendFinalDatagram()
